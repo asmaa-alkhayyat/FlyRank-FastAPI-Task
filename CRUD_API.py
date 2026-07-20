555fe87c7704e23d70d7b05e5cdbee6379a0d423
Asma'a Al-khayyat <as.alkhayyat@gmail.com>
2026-07-21 00:41:41 +0300

    Stage 2: read endpoints with 404

--- CRUD_API.py
+++ CRUD_API.py
@@ -1,7 +1,13 @@
-
+from fastapi.responses import JSONResponse
 from fastapi import FastAPI
 
 app = FastAPI()
+
+tasks = [
+    {"id": 1, "title": "Pen", "done": True},
+    {"id": 2, "title": "Book", "done": False},
+    {"id": 3, "title": "Eraser", "done": True}
+]
 
 @app.get("/")
 async def get():
@@ -10,3 +16,15 @@ async def get():
 @app.get("/health")
 async def get_health():
   return {"status": "ok"}
+
+
+@app.get("/tasks")
+async def get_tasks():
+  return tasks
+
+@app.get("/tasks/{id}")
+async def get_tasks_id(id:int):
+  for task in tasks:
+    if task["id"] == id:
+      return task
+  return JSONResponse(status_code=404, content={"error": f"Task {id} not found"})
